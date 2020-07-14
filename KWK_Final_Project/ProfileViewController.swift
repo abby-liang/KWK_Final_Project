a65334807e4d1dd83a5a47d8cf82a38ade9d0678
Kimberly <kimberly.m.sejas@gmail.com>
2020-07-14 14:36:19 -0400

    updated version

---
+++ KWK_Final_Project/ProfileViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ProfileViewController.swift
+//  KWK_Final_Project
+//
+//  Created by Kimberly Sejas on 7/14/20.
+//  Copyright © 2020 Sophia Wu. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBAction func DoneButton(_ sender: UIButton) {
+    }
+
+    
+    @IBOutlet weak var goal: UITextField!
+    
+    @IBOutlet weak var name: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        
+        // Do any additional setup after loading the view.
+   
+    
+    
+    
+    
+    
+    
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- KWK_Final_Project/ViewController.swift
+++ KWK_Final_Project/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func supervisor(_ sender: UIButton) {
+    }
+    
+    @IBAction func image(_ sender: UIButton) {
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
